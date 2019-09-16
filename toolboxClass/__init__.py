4e886b52fc0a62c0c8862a6f0abc28fa5d445c27
SSchramm <sebschramm@yahoo.de>
2019-09-16 10:13:21 +0200

    Update __init__.py

--- toolboxClass/__init__.py
+++ toolboxClass/__init__.py
@@ -1,10 +1,14 @@
-from toolboxClass import _GUI, _Load, _Popups, _Update, _Calibration, _Plot, _Export, _Delete
+from toolboxClass import _GUI, _Load, _Popups, _Update, _Calibration, _Plot,\
+                         _Export, _Delete
 
-class MRTCalibrationToolbox(_GUI.Mixin, _Load.Mixin, _Popups.Mixin, _Update.Mixin, _Calibration.Mixin, _Plot.Mixin, _Export.Mixin, _Delete.Mixin,):
+
+class MRTCalibrationToolbox(_GUI.Mixin, _Load.Mixin, _Popups.Mixin,
+                            _Update.Mixin, _Calibration.Mixin, _Plot.Mixin,
+                            _Export.Mixin, _Delete.Mixin,):
     def __init__(self, master, *args, **kwargs):
-        import tkinter as tk
         self.master = master
-        self.screen_width = master.winfo_screenwidth()  # For two screens, divide by corresponding factor 2
+        # For two screens, divide by corresponding factor 2
+        self.screen_width = master.winfo_screenwidth()
         self.screen_height = master.winfo_screenheight()
         master.title("MRT Camera Calibration Toolbox")
         self.initialize_GUI_variables()
